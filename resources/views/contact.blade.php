b9c9bacd91606012b8d35ed148c7d6b8eecc5258
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-10 00:34:16 -0400

    auth working and a few fixes

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+
+@section('content')
+	<header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb">
+			<li><a href="index.html">Home</a></li>
+			<li class="active">Contact Us</li>
+		</ol>
+
+		<div class="row">
+			
+			<!-- Article main content -->
+			<article class="col-sm-9 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">Contact us</h1>
+				</header>
+				
+				<p>
+					We’d love to hear from you. Please fill out the following form and we will get in touch with you shortly.
+				</p>
+				<br>
+					<form>
+						<div class="row">
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Name">
+							</div>
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Email">
+							</div>
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Phone">
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-12">
+								<textarea placeholder="Type your message here..." class="form-control" rows="9"></textarea>
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-6">
+								<label class="checkbox"><input type="checkbox"> Sign up for newsletter</label>
+							</div>
+							<div class="col-sm-6 text-right">
+								<input class="btn btn-action" type="submit" value="Send message">
+							</div>
+						</div>
+					</form>
+
+			</article>
+			<!-- /Article -->
+
+			<!-- Sidebar -->
+			<aside class="col-sm-3 sidebar sidebar-right">
+
+				<div class="widget">
+					<h4>Phone</h4>
+					<phone>
+						(321) 200-5597
+					</phone><br>
+
+					<h4>Address</h4>
+					<address>
+						2538 Woods Edge Circle<br> Orlando, FL 32817
+					</address>
+					
+					<h4>Email</h4>
+					<email>
+						cs@myturnkeyhost.com
+					</email>
+				</div>
+
+			</aside>
+			<!-- /Sidebar -->
+
+		</div>
+	</div>	<!-- /container -->
+			
+
+		</div>
+	</div>	<!-- /container -->
+	
+	<section class="container-full top-space">
+		<div id="map"></div>
+	</section>
+
+@endsection
