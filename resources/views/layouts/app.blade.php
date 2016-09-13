d87d67306b6e9d3682eb4c9ecaf3b8eb32ac6f50
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-12 21:21:04 -0400

    font awesome fix - again.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="/css/app.css" rel="stylesheet">
     <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-    {{ HTML::style('css/font-awesome.min.css') }}
+    <link rel="stylesheet" href="/css/font-awesome.css">
 
     <!-- Stylesheets -->
     <link rel="stylesheet" href="/css/bootstrap-theme.css" media="screen" >
