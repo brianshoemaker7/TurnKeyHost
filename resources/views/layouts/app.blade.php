cd54d9ba7720e84d1853fcb64ce68e014ed37c76
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-12 20:58:12 -0400

    Changed link on App page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
                     @if (Auth::guest())
                       <li class="active"><a class="btn" href="login">SIGN IN / SIGN UP</a></li>
                     @else 
-                      <li class="active"><a href="#" > {{ Auth::user()->name }} </a></li>
+                      <li class="active"><a href="" > {{ Auth::user()->name }} </a></li>
                     @endif
                 </ul>
                 
