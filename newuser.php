7dc26db86fb681a3120feb57cd24cf67bde1563e
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-23 04:51:34 -0400

    working user add

--- newuser.php
+++ newuser.php
@@ -12,7 +12,7 @@ $name = $json[name]; $password = $json[password]; $domain = $json[domain];
 
 // echo '<pre>'.print_r(json_decode(file_get_contents("php://input")),1).'</pre>';
 
-exec("sudo useradd -p $password $name");
+exec("sudo useradd -d /home/$name -p $password $name");
 
 // exec('sudo chown -R '.$name.':www-data /var/www/html/'.$name.'/public_html/');
 
