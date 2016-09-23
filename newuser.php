b8f9d8af5c0d9234addfe727f7e9103e63c19e0e
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-23 01:57:40 -0400

    adding linux commands.

--- newuser.php
+++ newuser.php
@@ -1,32 +1,22 @@
 <?php
 
-
+//Create a variable for the results
 $result = file_get_contents("php://input");
 
+// Decode the results into another variable
 $json = json_decode($result, true);
 
-print_r($json);
-
-echo $json[name];
-
-$name = $json[name];
-
-echo $name;
+// Assign usable variables for json results
+$name = $json[name]; $password = $json[password]; $domain = $json[domain];
 
 // echo '<pre>'.print_r(json_decode(file_get_contents("php://input")),1).'</pre>';
 
-// $data = json_decode(file_get_contents("php://input")),1;
+exec('sudo adduser '.$name.' -m -d /var/www/html/'.$name.'/public_html/' );
 
-// echo $data
+exec($name.'.:.'.$password.' | chpasswd');
 
-// $name->name; $password->password; $domain->domain;
+exec('sudo chown -R '.$name.':www-data /var/www/html/'.$name.'/public_html/');
 
-// echo $name; echo $password; echo $domain;
+exec('sudo find /var/www/html/'.$name.'/public_html/ -type d -exec chmod 0755 {} \; -or -type f -exec chmod 0644 {} \;');
 
-// exec('sudo adduser '.$name.' -m -d /var/www/html/'.$name.'/public_html/' );
-
-// exec($name.'.:.'.$password.' | chpasswd');
-
-// exec('sudo chown -R '.$name.':www-data /var/www/html/'.$name.'/public_html/');
-
-// exec('sudo find /var/www/html/'.$name.'/public_html/ -type d -exec chmod 0755 {} \; -or -type f -exec chmod 0644 {} \;');
+echo "Everything worked";
