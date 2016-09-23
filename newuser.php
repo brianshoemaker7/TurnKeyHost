415864d5a49790eadf60368ad661cfbc3098f981
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-23 02:10:19 -0400

    adding linux commands.

--- newuser.php
+++ newuser.php
@@ -11,12 +11,14 @@ $name = $json[name]; $password = $json[password]; $domain = $json[domain];
 
 // echo '<pre>'.print_r(json_decode(file_get_contents("php://input")),1).'</pre>';
 
-exec('sudo adduser '.$name.' -m -d /var/www/html/'.$name.'/public_html/' );
+exec('sudo useradd -d /www/html/'.$name.'/public_html/ -p '.$password.' '.$name'');
 
-exec($name.'.:.'.$password.' | chpasswd');
+// exec('sudo adduser '.$name.' -m -d /var/www/html/'.$name.'/public_html/' );
 
-exec('sudo chown -R '.$name.':www-data /var/www/html/'.$name.'/public_html/');
+// exec($name.'.:.'.$password.' | chpasswd');
 
-exec('sudo find /var/www/html/'.$name.'/public_html/ -type d -exec chmod 0755 {} \; -or -type f -exec chmod 0644 {} \;');
+// exec('sudo chown -R '.$name.':www-data /var/www/html/'.$name.'/public_html/');
 
-echo "Everything worked";
+// exec('sudo find /var/www/html/'.$name.'/public_html/ -type d -exec chmod 0755 {} \; -or -type f -exec chmod 0644 {} \;');
+
+// echo "Everything worked";
