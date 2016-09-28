2fd292a1595f9858f999850e44c0816e96e49e8a
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-28 03:09:47 -0400

    Changed some content on the blades.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -21,10 +19,7 @@
     <link rel="stylesheet" href="/css/main.css">
 
     <!-- Scripts -->
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
+ 
     </script>
 </head>
 <body>
