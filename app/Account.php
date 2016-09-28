cabe4ffdd8e081e73121f9379c7abed8747d1bda
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-28 05:14:36 -0400

    Changed some content on the blades.

--- app/Account.php
+++ app/Account.php
@@ -14,6 +14,6 @@ class Account extends Model
      */
    	protected $primaryKey = 'id';
     protected $table = 'accounts';
-    protected $fillable = ['name', 'email', 'password', 'domain', 'street', 'city', 'state', 'zipcode'];
+    protected $fillable = ['name', 'email', 'password', 'domain', 'package'];
 
 };
