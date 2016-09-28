3a1455e09e7c912e19a242458c59e191efe95595
brianshoemaker7 <brianshoemaker7@gmail.com>
2016-09-28 03:51:57 -0400

    Changed some content on the blades.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,26 +79,17 @@
                         <div class="widget-body">
                             <p>321-200-5597<br>
                                 <a href="mailto:#">me@brianshoemaker.info</a><br>
-                                2538 Woods Edge Circle <br>Orlando, FL 32817
+                                <br>
+                                2538 Woods Edge Circle, Orlando, FL 32817
                             </p>    
                         </div>
                     </div>
 
-                    <div class="col-md-3 widget">
-                        <h3 class="widget-title">Follow me</h3>
+                    <div class="col-md-9 widget">
+                        <h3 class="widget-title">Quote of the Day:</h3>
                         <div class="widget-body">
-                            <p class="follow-me-icons clearfix">
-                                <a href=""><span class="fa fa-linkedin fa-2"></span></a>
-                                <a href=""><i class="fa fa-github fa-2"></i></a>
-                            </p>    
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 widget">
-                        <h3 class="widget-title">More Information</h3>
-                        <div class="widget-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
-                            <p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
+                            <p>our work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. <br><br>-Steve Jobs
+</p>
                         </div>
                     </div>
 
@@ -113,10 +104,10 @@
                     <div class="col-md-6 widget">
                         <div class="widget-body">
                             <p class="simplenav">
-                                <a href="/">Home</a> | 
-                                <a href="/plans.html">About</a> |
-                                <a href="/contact">Contact</a> |
-                                <b><a href="/register">Sign up</a></b>
+                                <a href="#">Home</a> | 
+                                <a href="/plans">Plans</a> |
+                                <a href="/contact">Contact</a> 
+                        
                             </p>
                         </div>
                     </div>
